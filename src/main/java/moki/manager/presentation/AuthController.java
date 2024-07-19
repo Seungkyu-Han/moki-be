5c3cf2c05c297426eeda6888492352adb8dc3586
Seungkyu-Han <trust1204@gmail.com>
2024-07-19 14:58:36 +0900

    feat: add AuthController

---
+++ src/main/java/moki/manager/presentation/AuthController.java
@@ -0,0 +1,9 @@
+package moki.manager.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+}
