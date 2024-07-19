2ce1f5ef2814563107fbb05218928859b394d382
Seungkyu-Han <trust1204@gmail.com>
2024-07-19 14:57:44 +0900

    feat: add UserEntity

---
+++ src/main/java/moki/manager/model/entity/User.java
@@ -0,0 +1,27 @@
+package moki.manager.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String loginId;
+
+    private String password;
+
+}
