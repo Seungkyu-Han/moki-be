f7423119369c947e6b98ae2f0159e6a004904c8b
Seungkyu-Han <trust1204@gmail.com>
2024-07-22 12:11:25 +0900

    feat: add Menu Entity

--- src/main/java/moki/manager/model/entity/SaleMenu.java
+++ src/main/java/moki/manager/model/entity/SaleMenu.java
@@ -1,10 +1,18 @@
 package moki.manager.model.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SaleMenu {
 
     @Id
